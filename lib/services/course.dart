704c62f54c49f66b2c1d8eb697d42592609ce9c6
phd91105 <phd051199@gmail.com>
2021-05-31 23:07:02 +0700

    set headers

--- lib/services/course.dart
+++ lib/services/course.dart
@@ -1,13 +1,20 @@
+import 'package:course_app/controllers/auth/login.dart';
 import 'package:course_app/models/course.dart';
 import 'package:course_app/utils/constants.dart';
+import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
 class CourseServices {
   static final client = http.Client();
+  static final LoginController loginController = Get.put(LoginController());
+  static final headers = {
+    'Authorization': 'Bearer ${loginController.jwt.value}'
+  };
 
   static Future<List<Course>> fetchCourses() async {
     final response = await client.get(
       Uri.parse('$apiURL/courses'),
+      headers: headers,
     );
     if (response.statusCode == 200) {
       final jsonString = response.body;
@@ -20,6 +27,7 @@ class CourseServices {
   static Future<List<Course>> fetchMyCourses() async {
     final response = await client.get(
       Uri.parse('$apiURL/mycourses'),
+      headers: headers,
     );
     if (response.statusCode == 200) {
       final jsonString = response.body;
